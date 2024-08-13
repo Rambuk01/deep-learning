1c1df56c476bf339474827ce7c10ddae19a2e919
Rambuk01 <festersenmario@gmail.com>
2024-08-13 13:51:49 +0200

    asd

--- exercise06/ex06.py
+++ exercise06/ex06.py
@@ -11,34 +11,25 @@ test_dataset = datasets.MNIST(root='./data', train=False, download=True, transfo
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
-neurons = 20
-class NeuralNet(nn.Module):
+class MyNetwork(nn.Module):
     def __init__(self):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(28*28, neurons)
-        self.fc2 = nn.Linear(neurons, neurons)
-        self.fc3 = nn.Linear(neurons, neurons)
-        self.fc4 = nn.Linear(neurons, neurons)
-        self.fc5 = nn.Linear(neurons, neurons)
+        super(MyNetwork, self).__init__()
+        self.fc1 = nn.Linear(in_features=28*28, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.fc3 = nn.Linear(in_features=64, out_features=10)
 
-        self.fc6 = nn.Linear(neurons, 10)
-    
+        self.relu = nn.ReLU()
+        
     def forward(self, x):
         x = torch.flatten(x, 1)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-
-        x = torch.relu(self.fc4(x))
-        x = torch.relu(self.fc5(x))
-
-
-        x = self.fc6(x)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
         return x
-
-# Regularization parameters
     
-model = NeuralNet()
+model = MyNetwork()
+print(model)
+
 criterion = nn.CrossEntropyLoss()
 
 optimizer = optim.SGD(model.parameters(), lr=0.01) #momentum=0.9
